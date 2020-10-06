f7421475bd80b8ba5b4d344b81162aa646c1f2d0
Rafael Barbosa <rafaelbarbosadc@gmail.com>
2020-10-05 20:13:03 -0400

    Change port

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ class App {
     this.middlewares();
     this.routes();
 
-    this.express.listen(80, () => console.log(`Server is running at 3001`));
+    this.express.listen(5050, () => console.log(`Server is running at 3001`));
   }
 
   database() {
